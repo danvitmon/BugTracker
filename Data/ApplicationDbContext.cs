2f50fa1e07ca8ca14aeca59f4b0b3eafb938cc6a
Daniel Monastirsky <danvitmon@gmail.com>
2023-05-24 16:14:34 -0700

    Fix Issues/Add Account Setup

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using BugTracker.Models;
 
 namespace BugTracker.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<BugTracker.Models.Company> Company { get; set; } = default!;
+        public DbSet<BugTracker.Models.Invite> Invite { get; set; } = default!;
+        public DbSet<BugTracker.Models.Notification> Notification { get; set; } = default!;
+        public DbSet<BugTracker.Models.NotificationType> NotificationType { get; set; } = default!;
+        public DbSet<BugTracker.Models.Project> Project { get; set; } = default!;
+        public DbSet<BugTracker.Models.Ticket> Ticket { get; set; } = default!;
+        public DbSet<BugTracker.Models.TicketAttachment> TicketAttachment { get; set; } = default!;
+        public DbSet<BugTracker.Models.TicketComment> TicketComment { get; set; } = default!;
+        public DbSet<BugTracker.Models.TicketHistory> TicketHistory { get; set; } = default!;
     }
 }
--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BugTracker.Models
 {
-    public class BTUser
+    public class BTUser : IdentityUser
     {
         [Required]
         [Display(Name = "First Name")]
